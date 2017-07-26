619229b8dc4ba5dc56dd71db50343662fec92959
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-07-26 16:35:09 +0200

    templates

--- app/routes/offers/controller.js
+++ app/routes/offers/controller.js
@@ -6,6 +6,7 @@ angular.module('wevelopersApp')
                 .then(function(response) {
                     console.log(response)
                     $scope.offerDetails = response;
+                    $location.path('/details')
                 })
         }
     })
