d6e966819db078ce5765c39f0357fd16cb02ca5a
Marc Rosell <Marc@MacBook-Pro-de-Marc.local>
2017-07-27 17:10:47 +0200

    design

--- js/routes/results/controller.js
+++ js/routes/results/controller.js
@@ -1,22 +1,22 @@
 angular.module('wevelopersApp')
-    .controller('SearchResultsController', function ($scope, $rootScope, infojobsFactory, $location) {
-      $scope.searchOffers = function (query) {
-        $rootScope.offers = []
-        $rootScope.notFound = ''
-        infojobsFactory.getOffersIngenieros(query)
-                .then(function (offersIngenieros) {
-                  infojobsFactory.getOffersInformatica(query)
-                        .then(function (offersInformatica) {
-                          offers = offersIngenieros.concat(offersInformatica)
-                          if (offers.length !== 0) {
-                            $rootScope.offers = offers
-                            $location.path('/search-results')
-                            console.log(offers)
-                          } else {
-                            $rootScope.notFound = 'No se han encontrado ofertas paras los parámetros de busqueda introducidos'
-                            $location.path('/search-results')
-                          }
+    .controller('SearchResultsController', function($scope, $rootScope, infojobsFactory, $location) {
+        $scope.searchOffers = function(query) {
+            $rootScope.offers = []
+            $rootScope.notFound = ''
+            infojobsFactory.getOffersIngenieros(query)
+                .then(function(offersIngenieros) {
+                    infojobsFactory.getOffersInformatica(query)
+                        .then(function(offersInformatica) {
+                            offers = offersIngenieros.concat(offersInformatica)
+                            if (offers.length !== 0) {
+                                $rootScope.offers = offers
+                                $location.path('/search-results')
+                                console.log(offers)
+                            } else {
+                                $rootScope.notFound = "We didn't find any result fitting whith your search 😔"
+                                $location.path('/search-results')
+                            }
                         })
                 })
-      }
+        }
     })
