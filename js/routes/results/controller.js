8b9a24cfaee2183a4e26f73bac105e75b540c46a
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-07-27 17:40:30 +0200

    Homecontroller

--- js/routes/results/controller.js
+++ js/routes/results/controller.js
@@ -34,24 +34,4 @@ angular.module('wevelopersApp')
                             $scope.notFound = 'No se han encontrado ofertas paras los parámetros de busqueda introducidos'
                             $location.path('/search-results' + query)
                           }
-    .controller('SearchResultsController', function($scope, $rootScope, infojobsFactory, $location) {
-        $scope.searchOffers = function(query) {
-            $rootScope.offers = []
-            $rootScope.notFound = ''
-            infojobsFactory.getOffersIngenieros(query)
-                .then(function(offersIngenieros) {
-                    infojobsFactory.getOffersInformatica(query)
-                        .then(function(offersInformatica) {
-                            offers = offersIngenieros.concat(offersInformatica)
-                            if (offers.length !== 0) {
-                                $rootScope.offers = offers
-                                $location.path('/search-results')
-                                console.log(offers)
-                            } else {
-                                $rootScope.notFound = "We didn't find any result fitting whith your search 😔"
-                                $location.path('/search-results')
-                            }
-                        })
-                })
-        }
-    })
+    }
